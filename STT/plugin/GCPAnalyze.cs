7dab0c28484ae1947041f022d4fbe519b5e4bb07
Pou Lin <poumason@live.com>
2020-12-15 23:49:39 +0800

    independency gcp analyize to plugin

---
+++ STT/plugin/GCPAnalyze.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Language.V1;
+
+namespace mp3WavConverter.STT.plugin
+{
+    public class GCPAnalyze
+    {
+        LanguageServiceClient client = LanguageServiceClient.Create();
+
+        public void Anaylize(string rawContent, string fileItem, string outFolder)
+        {
+            Dictionary<string, int> score = new Dictionary<string, int>();
+
+            var response = client.AnalyzeEntities(Document.FromPlainText(rawContent, "zh-Hant"));
+            Debug.WriteLine($"Detected language: {response.Language}");
+            Debug.WriteLine("Detected entities:");
+            foreach (Entity entity in response.Entities)
+            {
+                var key = $"{entity.Name}_{entity.Type}";
+                var exist = score.Keys.Where(x => x == key).FirstOrDefault();
+                if (exist == null)
+                {
+                    score.Add(key, 1);
+                }
+                else
+                {
+                    score[key] += 1;
+                }
+
+                Debug.WriteLine($"  {entity.Name} (type: {entity.Type})({(int)(entity.Salience * 100)}%)");
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var item in score.OrderByDescending(x => x.Value))
+            {
+                string[] format = item.Key.Split('_');
+                Debug.WriteLine($"{format[0]} , {format[1]}, count: {item.Value}");
+                builder.AppendLine($"{format[0]} , {format[1]}, count: {item.Value}");
+            }
+            var scoreFile = $"{Path.GetFileNameWithoutExtension(fileItem)}.score";
+            var newScoreFile = Path.Combine(outFolder, scoreFile);
+
+            File.WriteAllText(newScoreFile, builder.ToString());
+        }
+    }
+}
--- STT/v3/Stt3Helper.cs
+++ STT/v3/Stt3Helper.cs
@@ -1,6 +1,4 @@
-﻿using Google.Apis.Auth.OAuth2;
-using Google.Cloud.Language.V1;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,10 +16,6 @@ namespace mp3WavConverter.stt.v3
 
         public static void ParseContentV3(string outFolder, string[] jsonFiles)
         {
-            LanguageServiceClient client = LanguageServiceClient.Create();
-
-            Dictionary<string, int> score = new Dictionary<string, int>();
-
             foreach (var fileItem in jsonFiles)
             {
                 var content = File.ReadAllText(fileItem);
@@ -88,24 +82,8 @@ namespace mp3WavConverter.stt.v3
                             }
                         }
                     }
-                    // 4. google text analysis
-                    var response = client.AnalyzeEntities(Document.FromPlainText(rawContent, "zh-Hant"));
-                    Debug.WriteLine($"Detected language: {response.Language}");
-                    Debug.WriteLine("Detected entities:");
-                    foreach (Entity entity in response.Entities)
-                    {
-                        var key = $"{entity.Name}_{entity.Type}";
-                        var exist = score.Keys.Where(x => x == key).FirstOrDefault();
-                        if (exist == null)
-                        {
-                            score.Add(key, 1);
-                        }else
-                        {
-                            score[key] += 1;
-                        }
 
-                        Debug.WriteLine($"  {entity.Name} (type: {entity.Type})({(int)(entity.Salience * 100)}%)");
-                    }
+                    //// 4. google text analysis
 
                     // 3. when the VTT item is lasted, but end_at is empty, must feeding it.
                     if (tempVttItem != null && tempVttItem.EndAtMS == 0)
@@ -131,20 +109,6 @@ namespace mp3WavConverter.stt.v3
 
                     transcriptObject.sentences.Add(sentence);
                 }
-
-
-                StringBuilder builder = new StringBuilder();
-
-                foreach (var item in score.OrderByDescending(x=>x.Value))
-                {
-                    string[] format = item.Key.Split('_');
-                    Debug.WriteLine($"{format[0]} , {format[1]}, count: {item.Value}");
-                    builder.AppendLine($"{format[0]} , {format[1]}, count: {item.Value}");
-                }
-                var scoreFile = $"{Path.GetFileNameWithoutExtension(fileItem)}.score";
-                var newScoreFile = Path.Combine(outFolder, scoreFile);
-
-                File.WriteAllText(newScoreFile, builder.ToString());
 
                 var outputJson = JsonConvert.SerializeObject(transcriptObject);
 
